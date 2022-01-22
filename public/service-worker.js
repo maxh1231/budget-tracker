92a907dd40ddd98f5d3c6cacd08bd01a4fd52bd6
maxh1231 <maxhumpherys@gmail.com>
2022-01-22 14:12:13 -0700

    deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,14 @@ const FILES_TO_CACHE = [
     "./js/index.js",
     "./manifest.json",
     "./js/idb.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
 ];
 
 self.addEventListener('install', function (e) {
